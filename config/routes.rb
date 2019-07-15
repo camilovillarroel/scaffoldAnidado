71b86a429b36ef0c5dfc98381243a2329728ca3f
Camilo Villarroel Perez <camiloed92@hotmail.com>
2019-07-14 21:21:20 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   delete '/songs/:id',to: 'songs#destroy', as: 'delete_song'
   resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
